d8c5e1bf45f6f55e70e78a668779a3051b94b8be
tetsuya-stn <t.hitotsuishi@gmail.com>
2020-12-13 15:00:54 +0900

    Modelクラス, レイアウトページを新規作成

---
+++ ToDoList/Models/Todolist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+	public class Todolist
+	{
+		public int Id { get; set; }
+
+		[DisplayName("タスク")]
+		[Required]
+		public string Task { get; set; }
+
+		[DisplayName("期限")]
+		[Required]
+		public DateTime LimitDate { get; set; }
+
+		[DisplayName("優先度")]
+		public string Priority { get; set; }
+
+		[DisplayName("完了")]
+		public bool Done { get; set; }
+	}
+
+	public class TodolistsContext: DbContext
+	{
+		public DbSet<Todolist> Todolists { get; set; }
+	}
+}
